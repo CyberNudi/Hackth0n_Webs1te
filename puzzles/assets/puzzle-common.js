453aa2cc4d877fdc1bc04c64f13dd94f504543eb
Q3goldlegeng <tw123daniel03202132@gmail.com>
2026-03-06 22:42:29 +0800

    downlaodZipCompleted

--- puzzles/assets/puzzle-common.js
+++ puzzles/assets/puzzle-common.js
@@ -3,8 +3,21 @@
 //資料夾網址資訊
 const PUZZLE_CONFIG = {
   1: "1lw_hfqBiwkp7Hm9kSeCE2A_9eZvk4nh7",
-  2: "資料夾ID_2",
-  3: "資料夾ID_3"
+  2: "1N3rSYD2XceD72Me9Z8MFOsalNan-v_Pl",
+  3: "1Hx0EcUp_ngjOsY65X9IvY_CpRoc0-Ers",
+  4: "1btsLuEhax3qZeZudrXuIHFl1cmw17R4s",
+  5: "1ZWLIy1xqTvCW0mvXQOA6q45fFqzf11ow",
+  6: "1AOluloVVuyFTHtfyYscKamyy2JI9BiRm",
+  7: "1AOluloVVuyFTHtfyYscKamyy2JI9BiRm",//沒有這關
+  8: "115m9lX3RkeWegkQgJNFJo2UUFAcpX09If",
+  9: "1WGvu_u5tPRdTbPtU_loJJnM_9nH9myPQ",
+  10: "1lGor2q4xjmsPrr3sZZb_2H1TOwanSzfP",
+  11: "1Xso2I8J1okFQ80puLXTWott2Ah36AINO",
+  12: "17-BVeJFVqKSm7igrdakVRcs_bCntk1Qb",
+  13: "1mM_-2K3yXNGUgkKEqGd0EU4bncJzUKB9",
+  14: "11vtzN9uPoY0gqz6GlUFnXJ5ZKh4dKyRz",
+  15: "1lSN0y8D-quXm1GiVCIwU4-L4RhzscR-",
+  16: "1X1TefpVfbixYfcCIbjv0KocKk16ywDJc"
 };
 
 
